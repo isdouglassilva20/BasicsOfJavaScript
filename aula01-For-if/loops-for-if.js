3c36cbb9880b74fc560f57dc1ecc3fa7acdddf6a
isdouglassilva20 <ismaeldouglas.silva20@gmail.com>
2020-07-24 17:37:15 -0300

    Delete loops-for-if.js

--- aula01-For-if/loops-for-if.js
+++
@@ -1,9 +0,0 @@
-for (let cont=0; cont<100; cont++) {
-    if(cont % 3 === 0 && cont % 5 ===0){
-        console.log(`${cont} é multiplo de 3 e 5`)
-    } else if (cont % 3 === 0){
-        console.log(`${cont} é multiplo de 3`)
-    } else if(cont % 5 === 0){
-        console.log(`${cont} é multiplo de 5`)
-    }
-    }
